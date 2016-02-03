bbf470ee6ab4696c06cb0dc36b1c1e0cc6ff7b09
Nick Kenens <nick@nickkenens.com>
2016-02-03 21:03:18 +0100

    Add Auth, Login & Create User Functionality

--- client/app.js
+++ client/app.js
@@ -1,3 +1,19 @@
 Template.messages.helpers({
-	messages: Messages
+	messages: Messages.find({})
+})
+
+Template.footer.events({
+
+
+	'keypress input': function(event) {
+		// responds to enter keypress (13)
+		// Appens html for the time being
+		if (event.charCode == 13) {
+			event.stopPropagation();
+			var message = event.target.value;
+			event.target.value ="";
+			Messages.insert({text: message});
+			return false;
+		}
+	}
 })
