d7572c7d95c758c9146ef82187e5476add4e767d
Nick Kenens <nick@nickkenens.com>
2016-02-03 21:07:35 +0100

    Add custom user functionality, github oauth & named posting

--- client/app.js
+++ client/app.js
@@ -17,3 +17,23 @@ Template.footer.events({
 		}
 	}
 })
+
+Template.registerHelper("usernameFromId", function(userId){
+	var user = Meteor.user.findOne({_id: userId});
+	if (typeof user === undefined) {
+		return "Anonymous"
+	}
+	if (typeof user.services.github !== "undefined") {
+		return user.services.github.username;
+	}
+
+	return user.username;
+});
+
+Template.registerHelper("timestampToTime", function (timestamp) {
+    var date = new Date(timestamp);
+    var hours = date.getHours();
+    var minutes = "0" + date.getMinutes();
+    var seconds = "0" + date.getSeconds();
+    return hours + ':' + minutes.substr(minutes.length-2) + ':' + seconds.substr(seconds.length-2);
+});
